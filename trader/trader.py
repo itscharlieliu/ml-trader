59916b26e319dbb777ca2917ec0208f68a4153fc
Charlie <itscharlieliu@gmail.com>
2020-09-01 16:50:48 -0700

    Plot moving avg

--- trader/trader.py
+++ trader/trader.py
@@ -1,13 +1,17 @@
 import datetime
 
+import matplotlib
+import pandas_datareader.data as web
+
 import yfinance
+from matplotlib import style, pyplot
 from sklearn.model_selection import train_test_split
 
 from iex.cloud import cloud
 
 
 class Trader:
-    def __init__(self, api_key: str, ticker: str, sandbox: bool = True, years: int = 1):
+    def __init__(self, api_key: str, ticker: str, sandbox: bool = True, years: int = 5):
         self._data_points = []
         self._cloud = cloud(api_key, sandbox)
         self._years = years
@@ -23,6 +27,27 @@ class Trader:
         print(f"{curr_date} {start_date}")
         self._data_points = yfinance.download(self._ticker, start_date, curr_date)
         print(self._data_points)
+
+        # Calculate rolling mean
+        close_price = self._data_points["Adj Close"]
+        moving_avg = close_price.rolling(window=100).mean()
+
+        print(moving_avg)
+
+        matplotlib.rc('figure', figsize=(8, 7))
+
+        style.use("ggplot")
+
+        close_price.plot(label="AAPL")
+        moving_avg.plot(label="Moving avg")
+
+        pyplot.legend()
+
+        pyplot.show()
+
+
+        # data_file = web.DataReader("AAPL", "yahoo", start_date, curr_date)
+        # print(data_file)
 
         # x = self._data_points[:, 0:4]
         # y = self._data_points[:, 4]
